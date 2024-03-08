5ef0b7d92acbb536d77c8675c4cb24e31422e1d8
Vyom Yadav <73882557+Vyom-Yadav@users.noreply.github.com>
2024-03-08 18:17:16 +0530

    Add basic demo app logic (#1)
    Signed-off-by: Vyom-Yadav <jackhammervyom@gmail.com>

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM golang:1.21.5 AS build
+
+WORKDIR /app
+
+COPY . .
+
+RUN go build -o hello-world .
+
+FROM alpine:3.19.1
+
+COPY --from=build /app/hello-world /
+COPY config.yaml /
+
+CMD ["/hello-world"]
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+func main() {
+	var greeting string
+
+	rootCmd := &cobra.Command{
+		Use:   "hello",
+		Short: "Prints a greeting",
+		Run: func(cmd *cobra.Command, args []string) {
+			if greeting != "" {
+				fmt.Println(greeting)
+			} else {
+				fmt.Println(viper.GetString("greeting"))
+			}
+		},
+	}
+
+	rootCmd.Flags().StringVarP(&greeting, "greeting", "g", "", "Greeting message")
+
+	cobra.OnInitialize(initConfig)
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		viper.SetConfigName("config")
+		viper.AddConfigPath(".")
+		viper.SetConfigType("yaml")
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		fmt.Println("Config file not found...")
+	}
+}
